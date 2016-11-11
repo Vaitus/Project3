37d64a91018fd2671215df05ec4dacf48d816822
Vaitus <vaitus666@gmail.com>
2016-11-11 07:34:47 +0100

    Add files via upload

--- main.f95
+++ main.f95
@@ -7,24 +7,24 @@ program main
     real :: x(3),v(3) !misto a rychlost castice
     real :: B(3), E(3) !pole pro mag. indukci a E. pole
     real :: q = -1.602e-19, m = 9.109e-31 !naboj a hmotnost elektronu
+    integer :: i
 
+    dt = 4e-11
     x = startingPositionForX()
     v = startingVelocity()
     B = Bindukce(x)
     E = Efield(x)
 
-    updateVelocity(x,v,E,B,-0.5*dt) !posunuti castice o 0.5 "dozadu"
+    call updateVelocity(x,v,E,B,-0.5) !posunuti castice o 0.5 "dozadu"
 
     do i = 0, 1000
         E = Efield(x)
         B = Bindukce(x)
 
-        updateVelocity(x, v, E, B, dt)
-        particlePush(x, v, dt)
+        call updateVelocity(x, v, E, B, 1.0)
+        call particlePush(x, v)
 
     end do
-
-    write (*,*) "Input number of points to average:", q
 
 contains
     !nastaveni startovni pozice
@@ -62,7 +62,7 @@ contains
     end function cross_product
 
     !posunuti castice o rychlost
-    subroutine particlePush(x, v, dt)
+    subroutine particlePush(x, v)
         real :: x(3)
         real :: v(3)
         real :: dt
@@ -73,9 +73,49 @@ contains
     end subroutine particlePush
 
     !upraveni rychlosti
- !   subroutine updateVelocity(x, v, E, B, dt)
+    subroutine updateVelocity(x, v, E, B, multi)
+        real :: x(3), v(3), E(3), B(3), multi
+        real :: v_minus(3), v_prime(3), v_plus(3)
+        real :: t(3), s(3), t_norma
+        real :: v_minus_crossproduct_t(3), v_prime_cross_s(3)
+        integer :: i
 
-  !  end subroutine updateVelocity
+        !vektor t
+        do i = 1, 3
+            t(i) = q/m * B(i)*0.5*dt*multi
+        end do
+
+        !norma vektoru t
+        t_norma = t(1)*t(1) + t(2)*t(2) + t(3)*t(3)
+
+        !vektor 2t/1+t^2
+        do i = 1, 3
+            s(i) = 2*t(i)/(1+t_norma)
+        end do
+
+        !v_minus
+        do i = 1, 3
+            v_minus(i) = v(i) + q/m * E(i) * 0.5*dt*multi
+        end do
+
+        !v_prime
+        v_minus_crossproduct_t = cross_product(v_minus, t)
+        do i = 1, 3
+            v_plus(i) = v_minus(i) + v_minus_crossproduct_t(i)
+        end do
+
+        !v_plus
+        v_prime_cross_s = cross_product(v_prime, s)
+        do i = 1, 3
+            v_plus(i) = v_minus(i) + v_prime_cross_s(i)
+        end do
+
+        !v^n+1/2
+        do i = 1, 3
+            v(i) = v_plus(i) + q/m * E(i) * 0.5*dt*multi
+        end do
+
+    end subroutine updateVelocity
 
     !nastaveni magneticke indukce
     function Bindukce(x) result(out)
